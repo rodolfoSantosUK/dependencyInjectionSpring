5cda77004810daef90fc67f86df6b276a22cbd90
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-11-10 14:47:47 -0300

    Uso de qualifiers

---
+++ src/main/java/guru/springframework/services/PropertyInjectedGreetingService.java
@@ -0,0 +1,13 @@
+package guru.springframework.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyInjectedGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - Property";
+    }
+
+}
--- src/main/java/guru/springframework/spring5WebApp/Spring5WebAppApplication.java
+++ src/main/java/guru/springframework/spring5WebApp/Spring5WebAppApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5WebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,7 +14,24 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Spring5WebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5WebAppApplication.class, args);
-	}
+		ApplicationContext ctx = SpringApplication.run(Spring5WebAppApplication.class, args);
 
+		guru.springframework.controllers.MyController myController = (guru.springframework.controllers.MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		guru.springframework.controllers.PropertyInjectedController propertyInjectedController = (guru.springframework.controllers.PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("--------- Setter");
+		guru.springframework.controllers.SetterInjectedController setterInjectedController = (guru.springframework.controllers.SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("-------- Constructor" );
+		guru.springframework.controllers.ConstructorInjectedController constructorInjectedController = (guru.springframework.controllers.ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
 }
